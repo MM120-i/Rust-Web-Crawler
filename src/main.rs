4af1a3612674a721f42b39fb044bdbbde0b6fa98
Annabel <annabel.rch31@gmail.com>
2026-08-11 16:31:13 -0400

    basic code and testing code rabbit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+// basic web crawler
+// https://books.toscrape.com/ or https://quotes.toscrape.com/ for testing
+// https://www.hellointerview.com/learn/system-design/problem-breakdowns/web-crawler
+
+
+// only returns links of original page -> no crawling involved (just surface level)
+// this was just a test 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let url = "https://books.toscrape.com/";
+
+    // fetch the page
+    // get request for Future, ? bails and returns error on error.
+    let body = reqwest::get(url).await?.text().await?;
+    println!("Fetched {} bytes", body.len());
+
+    // parse it and pull out every link
+    // doc is an html value now, we pass & body so body is not consumed
+    let doc = scraper::Html::parse_document(&body);
+    let selector = scraper::Selector::parse("a[href]").unwrap();
+
+    for element in doc.select(&selector) {
+        if let Some(href) = element.value().attr("href") {
+            println!("{}", href);
+        }
+    }
+    Ok(())
 }
+
